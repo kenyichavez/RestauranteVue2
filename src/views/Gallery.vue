5fe4e4d687d29d4ed5e29fdde54a1df4d657e933
kenyichavez <chavezkenyi2@gmail.com>
2022-05-24 23:18:31 -0500

    inicio

---
+++ src/views/Gallery.vue
@@ -0,0 +1,75 @@
+<template>
+    
+  <div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col ">
+    <div class="card bg-success">
+        <div class="w-25">
+            <img src="@/assets/amor.jpg" class="card-img-top  " alt="..." >
+        </div>
+      
+      <div class="card-body">
+        <h5 class="card-title">TARJETA DE AMOR</h5>
+        <p class="card-text">Te amo mucho mi poshito, eres alguien especial para mi y quiero que seas la ultima te AMO.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card bg-success">
+      <div class="w-25">
+            <img src="@/assets/amor.jpg" class="card-img-top  " alt="..." >
+        </div>
+      <div class="card-body">
+        <h5 class="card-title">TARJETA DE AMOR</h5>
+        <p class="card-text">Te amo mucho mi poshito, eres alguien especial para mi y quiero que seas la ultima te AMO.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card bg-success">
+      <div class="w-25">
+            <img src="@/assets/amor.jpg" class="card-img-top  " alt="..." >
+        </div>
+      <div class="card-body">
+        <h5 class="card-title">TARJETA DE AMOR</h5>
+        <p class="card-text">Te amo mucho mi poshito, eres alguien especial para mi y quiero que seas la ultima te AMO.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card bg-success">
+      <div class="w-25">
+            <img src="@/assets/amor.jpg" class="card-img-top  " alt="..." >
+        </div>
+      <div class="card-body">
+        <h5 class="card-title">TARJETA DE AMOR</h5>
+        <p class="card-text">Te amo mucho mi poshito, eres alguien especial para mi y quiero que seas la ultima te AMO.</p>
+      </div>
+    </div>
+  </div>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Gallery',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div >
+    
+    <Carrousel />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Carrousel from '@/components/Carrousel.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Carrousel,
+    
+  }
+}
+</script>
